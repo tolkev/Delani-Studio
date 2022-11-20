4af127f2732ebff6e4bc4fed237951a7a0836fae
tolkev <kelvinndungi5@gmail.com>
2022-11-20 22:00:54 +0300

    business logic

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+   
+    $('#design').click(function () {
+        $('#design-content').show(function () {
+            $('#design').hide('fast');
+        });
+    });
+    $('#design-content').click(function () {
+        $('#design').show(function () {
+            $('#design-content').hide('fast');
+        });
+    });
+    
