97b0cf42dd119e75803e7a818f7acb5364326f06
tolkev <kelvinndungi5@gmail.com>
2022-11-20 22:01:47 +0300

    another function

--- js/script.js
+++ js/script.js
@@ -11,4 +11,28 @@ $(document).ready(function () {
             $('#design-content').hide('fast');
         });
     });
+
+
+    $('#dev').click(function () {
+        $('#dev-content').show(function () {
+            $('#dev').hide('fast');
+        });
+    });
+    $('#dev-content').click(function () {
+        $('#dev').show(function () {
+            $('#dev-content').hide('fast');
+        });
+    });
+
+    $('#product').click(function () {
+        $('#product-content').show(function () {
+            $('#product').hide('fast');
+        });
+    });
+    $('#product-content').click(function () {
+        $('#product').show(function () {
+            $('#product-content').hide('fast');
+        });
+    });
+
     
