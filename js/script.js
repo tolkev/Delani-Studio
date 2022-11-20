601074e562502181f6ec63e67b344aa704be1275
tolkev <kelvinndungi5@gmail.com>
2022-11-20 22:02:26 +0300

    work8

--- js/script.js
+++ js/script.js
@@ -35,4 +35,53 @@ $(document).ready(function () {
         });
     });
 
-    
+    $('#work1').mouseenter(function () {
+        $('.work-heading').filter('#work1-title').show("slow");
+    }).mouseleave(function () {
+        $('#work1-title').hide("slow");
+    });
+
+
+    $('#work2').mouseenter(function () {
+        $('.work-heading').filter('#work2-title').show("slow");
+    }).mouseleave(function () {
+        $('#work2-title').hide("slow");
+    });
+
+    $('#work3').mouseenter(function () {
+        $('.work-heading').filter('#work3-title').show("slow");
+    }).mouseleave(function () {
+        $('#work3-title').hide("slow");
+    });
+
+
+    $('#work4').mouseenter(function () {
+        $('.work-heading').filter('#work4-title').show("slow");
+    }).mouseleave(function () {
+        $('#work4-title').hide("slow");
+    });
+
+    $('#work5').mouseenter(function () {
+        $('.work-heading').filter('#work5-title').show("slow");
+    }).mouseleave(function () {
+        $('#work5-title').hide("slow");
+    });
+
+
+    $('#work6').mouseenter(function () {
+        $('.work-heading').filter('#work6-title').show("slow");
+    }).mouseleave(function () {
+        $('#work6-title').hide("slow");
+    });
+
+    $('#work7').mouseenter(function () {
+        $('.work-heading').filter('#work7-title').show("slow");
+    }).mouseleave(function () {
+        $('#work7-title').hide("slow");
+    });
+    $('#work8').mouseenter(function () {
+        $('.work-heading').filter('#work8-title').show("slow");
+    }).mouseleave(function () {
+        $('#work8-title').hide("slow");
+    });
+});
